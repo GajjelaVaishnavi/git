9f2d35415358b6b0f4c1de8878d811f24942be24
GajjelaVaishnavi <141477880+GajjelaVaishnavi@users.noreply.github.com>
2025-03-13 11:35:00 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello")
+name=input()
+print(f"hi {name}") # formatted string
+print("hi",name)
+print(type(name))
+import pandas as pd  # type: ignore
+pd=input(int())
+print(pd)
+import numpy as np # type: ignore
+np={1,2,3}
+print(np)
